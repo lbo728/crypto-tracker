a20943a488d80c7ede01466a96975707ccbcb602
byungsker <extreme93@naver.com>
2023-09-26 09:34:48 +0900

    #5.9 React Query part One

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -116,16 +116,36 @@ interface PriceData {
   beta_value: number;
   first_data_at: string;
   last_updated: string;
-  quotes: object;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const location = useLocation();
-  const state = location.state as RouteState;
-  const { coinId } = useParams<RouteParams>();
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const location = useLocation();
+  const { coinId } = useParams<RouteParams>();
+  const state = location.state as RouteState;
   const priceMatch = useMatch("/:coinId/price");
   const chartMatch = useMatch("/:coinId/chart");
   console.log("priceMatch", priceMatch);
